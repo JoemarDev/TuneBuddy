c16afaa4990bb1e0f1712378dfb0e4a0561ab30c
joemardev <joemarb.dev@gmail.com>
2022-11-06 19:51:37 +0800

    Update Home Page Loader

--- src/components/home-display/home-display.placeholder.jsx
+++ src/components/home-display/home-display.placeholder.jsx
@@ -3,19 +3,19 @@ const HomeDisplayPlaceholder = () => {
     return (
         <>
             <AppLoader className="w-72 h-9 mb-10 rounded-md"/>
-            <div className="grid grid-cols-5 gap-4 mt-5 ">
+            <div className="grid grid-cols-5 gap-4 mt-5 max-2xl:grid-cols-3 max-md:grid-cols-2 ">
                 {[0,0,0,0,0].map((i,x) =>  <AppLoader key={x} className="w-full h-56 rounded-3xl mb-10"/> )}
             </div>
             <AppLoader className="w-72 h-9 mb-10 rounded-md"/>
-            <div className="grid grid-cols-5 gap-4 mt-5 ">
+            <div className="grid grid-cols-5 gap-4 mt-5 max-2xl:grid-cols-3 max-md:grid-cols-2 ">
                 {[0,0,0,0,0].map((i,x) =>  <AppLoader key={x} className="w-full h-56 rounded-3xl mb-10"/> )}
             </div>
             <AppLoader className="w-72 h-9 mb-10 rounded-md"/>
-            <div className="grid grid-cols-5 gap-4 mt-5 ">
+            <div className="grid grid-cols-5 gap-4 mt-5 max-2xl:grid-cols-3 max-md:grid-cols-2 ">
                 {[0,0,0,0,0].map((i,x) =>  <AppLoader key={x} className="w-full h-56 rounded-3xl mb-10"/> )}
             </div>
             <AppLoader className="w-72 h-9 mb-10 rounded-md"/>
-            <div className="grid grid-cols-5 gap-4 mt-5 ">
+            <div className="grid grid-cols-5 gap-4 mt-5 max-2xl:grid-cols-3 max-md:grid-cols-2 ">
                 {[0,0,0,0,0].map((i,x) =>  <AppLoader key={x} className="w-full h-56 rounded-3xl mb-10"/> )}
             </div>
            
