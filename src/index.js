38dfb208afba32cf24189d2dc514a36050f7bd83
joemardev <joemarb.dev@gmail.com>
2022-11-06 18:36:19 +0800

    Register PWA

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ root.render(
   // </React.StrictMode>
 );
 
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
